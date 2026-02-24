149c6c1058f7e6b42d92dd4f712f82c059db3067
Dwnl <danielccbcoelho@gmail.com>
2026-02-24 17:11:43 -0300

    commit

--- src/Maratona_java_introducao/Aula07EstruturasDeRepetição.java
+++ src/Maratona_java_introducao/Aula07EstruturasDeRepetição.java
@@ -2,19 +2,21 @@ package Maratona_java_introducao;
 
 public class Aula07EstruturasDeRepetição {
     public static void main(String[] args) {
-        //while [Executa apenas se a condição for atendida], do while [Executa pelo menos uma vez mesmo se a condição ainda nao estiver sendo atendida], for
+        //while [Testa a condição antes de executar o bloco]
+        //do while [Executa o bloco primeiro e só depois verifica a condição.]
+        //for [Utilizado para contagens Indice: int i, Ate onde ela vai, incremento: i++]
         int count = 0;
         while(count <= 10){
-            System.out.println(count); //Se for ++count imprime apenas de 1 a 10
+            System.out.println(count);//Se for ++count imprime apenas de 1 a 10
             count++;
         }
 
-        count = 0; //resetando o valor do count para o do while funcionar
+        count = 0;//resetando o valor do count para o do while funcionar
         do{
             System.out.println("Dentro do while "+ ++count);
         }while(count < 10);
 
-        for(int i = 0 ; i < 10 ; i++){ //Usado para contagens, dividido em 3 partes: Indice: int i, Ate onde ela vai, incremento: i++
+        for(int i = 0 ; i < 10 ; i++){
             System.out.println("For numero: " + i);
         }
     }

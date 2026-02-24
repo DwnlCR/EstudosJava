58b6cc9adfcf6dc9a762353d27ca64caf29c4e8f
Dwnl <danielccbcoelho@gmail.com>
2026-02-24 15:09:50 -0300

    commit

--- src/Maratona_java_introducao/Aula02LeituraDeDados.java
+++ src/Maratona_java_introducao/Aula02LeituraDeDados.java
@@ -1,10 +1,11 @@
 package Maratona_java_introducao;
-
 import java.util.Scanner; //Classe responsavel por ler o que foi digitado no terminal
 
 public class Aula02LeituraDeDados {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);//input é a variavel que vai receber algum valor ou qualquer outro conteudo para que depois esse conteudo seja atribuido a uma variavel utilizavel, System.in é um objeto passado por parametro que a principio não deve ser mencionado
+        //input é a variavel que vai receber algum valor ou qualquer outro conteudo para que depois esse conteudo seja atribuido a uma variavel utilizavel
+        //System.in é um objeto passado por parametro que a principio não deve ser mencionado
+        Scanner input = new Scanner(System.in);
         System.out.println("Digita teu nome ae: ");
         String nome = input.next(); //A variavel declarada agora do tipo String vai receber apenas uma palavra advinda do terminal, next(): apenas 1 palavra
         System.out.println("Oia ai o 1° nome q tu digitou: " + nome);
@@ -17,7 +18,7 @@ public class Aula02LeituraDeDados {
         System.out.println("Olha só vc tem " + idade + " anos, q bacana");
         input.nextLine(); //limpar buffer
         System.out.println("Agora digite seu sexo M ou F: ");
-        char sexo = input.next().charAt(0); //Assim a gene pega apenas o primeiro caractere da primeira palavra
+        char sexo = input.next().charAt(0); //Assim a "sexo" pega apenas o primeiro caractere da primeira palavra
         System.out.println("Hmmm sexo "+sexo+" então, entendi");
     }
 }

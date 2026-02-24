9e9a269d2dd133df79d58ec3b36e386543713e3f
Dwnl <danielccbcoelho@gmail.com>
2026-02-24 14:53:54 -0300

    commit

--- src/Maratona_java_introducao/Aula03TiposPrimitivosExercicios.java
+++ src/Maratona_java_introducao/Aula03TiposPrimitivosExercicios.java
@@ -1,12 +1,9 @@
 package Maratona_java_introducao;
-
 import java.util.Scanner;
-
 public class Aula03TiposPrimitivosExercicios {
     public static void main(String[] args){
         /*
         Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
-
         Eu <nome>, morando no endereço <>, confirmo que recebi o salario de <salario>, na data <data>
          */
         Scanner scanner= new Scanner(System.in);
@@ -16,24 +13,20 @@ public class Aula03TiposPrimitivosExercicios {
         System.out.println("Digite seu endereço: ");
         String endereco = scanner.nextLine();
         System.out.println("Digite seu Salario: ");
-        String strsalario = scanner.nextLine();
-        Double salario = Double.parseDouble(strsalario);
+        String strSalario = scanner.nextLine();
+        Double salario = Double.parseDouble(strSalario);
         System.out.println("Digite a data de recebimento do salario: ");
         System.out.println("Dia: ");
-        String strdia = scanner.nextLine();
-        int dia = Integer.parseInt(strdia);
+        String strDia = scanner.nextLine();
+        int dia = Integer.parseInt(strDia);
         System.out.println("Mes: ");
-        String strmes = scanner.nextLine();
-        int mes = Integer.parseInt(strmes);
+        String strMes = scanner.nextLine();
+        int mes = Integer.parseInt(strMes);
         System.out.println("Ano: ");
-        String strano = scanner.nextLine();
-        int ano = Integer.parseInt(strano);
-
+        String strAno = scanner.nextLine();
+        int ano = Integer.parseInt(strAno);
         System.out.printf("Eu <%s>, morando no endereço <%s>, confirmo que recebi o salario de <%.2f>, na data <%02d/%02d/%d>%n",nome, endereco, salario,dia,mes,ano);
 
         scanner.close();
-
-
-
     }
 }

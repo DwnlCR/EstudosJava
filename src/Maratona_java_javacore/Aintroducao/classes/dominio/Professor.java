1c6bc7f681eaf7531a694e9ebcc260dad8325ac5
Dwnl <danielccbcoelho@gmail.com>
2026-02-25 17:16:24 -0300

    commit

--- src/Maratona_java_javacore/Aintroducao/classes/dominio/Professor.java
+++ src/Maratona_java_javacore/Aintroducao/classes/dominio/Professor.java
@@ -4,7 +4,6 @@ public class Professor {
     public String nome;
     public int idade;
     public char sexo;
-    //Pronto, agora sim, temos uma classe propria para tratar as informações de um professor
+    //Temos uma classe propria para tratar as informações de um professor
     //em um escopo ou forma para professores,
-    //o codigo esta coeso: mas agora vamos fazer o Professortest pata testar frequentemente o codigo relacionado aos professores
 }

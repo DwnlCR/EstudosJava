f1a7e765c0c2c4f2f2f5bf1fbe6dd11eb4bab007
Dwnl <danielccbcoelho@gmail.com>
2026-02-25 14:45:59 -0300

    commit

--- src/Maratona_java_introducao/Aula08Arrays03Iteração.java
+++ src/Maratona_java_introducao/Aula08Arrays03Iteração.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Aula08Arrays03Iteração {
     public static void main(String[] args) {
         String[] nomes = new String[9];
-        for(int i = 0; i < 3; i++){//Não Dinamico -> se o numero de pessoas aumentar o for não acompanha esse aumento, ou seja, ou falta espaço ou sobra espaço
+        //Não Dinamico -> se o numero de pessoas aumentar o for não acompanha esse aumento
+        //for não acompanha o array por faltar ou sobrar repetições pelo indice não exato
+        for(int i = 0; i < 3; i++){
             Scanner input = new Scanner(System.in);
             System.out.println("Digite o nome da pessoa da posição "+i+": ");
             nomes[i] = input.nextLine();
         }
-        for(int i = 0; i < nomes.length; i++){//Dinamico -> nomess.length mostra exatamente a quantidade de pessoas dentro do array, ou seja, o for acompanha o array
+        //Dinamico -> nomes.length mostra exatamente a quantidade de pessoas dentro do array
+        //for acompanha o array pois o indice é exato
+        for(int i = 0; i < nomes.length; i++){
             System.out.println("Nome da pessoa da posição "+i+": "+nomes[i]);
         }
     }

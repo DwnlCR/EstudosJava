9bc0db5f30d8e6f473e1de23d84f89d79081f9af
Dwnl <danielccbcoelho@gmail.com>
2026-02-26 19:51:33 -0300

    commit

--- src/Maratona_java_javacore/Bintroducao/metodos/dominio/Estudante.java
+++ src/Maratona_java_javacore/Bintroducao/metodos/dominio/Estudante.java
@@ -1,15 +1,21 @@
 package Maratona_java_javacore.Bintroducao.metodos.dominio;
-
 public class Estudante {
-        //Parametros tipo primitivo -> Parametros tipo referencia = AQUI A BAIXO:
+        //Parametros tipo primitivo -> Parametros tipo referencia
         public String nome;
         public int idade;
         public char sexo;
 
         public void imprime(){
-                //Agora sim vamos explicar o que é o comando this, para que serve? onde vive? de que se alimenta? é isso que veremos agora hehe
-                //Basicamente o this significa "esse", por exemplo: "que cor é esse papel?", ou seja this.papel = cor, ai alguem responde, o papel é branco, ou seja, cor = "Branco", então this.papel = "Branco".
-                //O this funciona como uma referencia dentro do objeto, mas para ele funcionar ele deve estar dentro da classe no qual vai buscar o valor a ser recebido
+                //Agora sim vamos explicar o que é o comando this
+                //para que serve? onde vive? de que se alimenta?
+                //é isso que veremos agora hehe
+                //Basicamente o this significa "esse"
+                //por exemplo: "que cor é esse papel?", ou seja this.papel = cor
+                //ai alguem responde, o papel é branco
+                //ou seja, cor = "Branco", então this.papel = "Branco".
+                //O this funciona como uma referencia dentro do objeto
+                //mas para ele funcionar onde ele deve estar?
+                //dentro da classe no qual vai buscar o valor a ser recebido
                 System.out.println(this.nome); //"Esse" nome é
                 System.out.println(this.idade); //"Essa" idade é
                 System.out.println(this.sexo); //"Esse" sexo é
--- src/Maratona_java_javacore/Bintroducao/metodos/test/EstudanteTest02_MetodosReferenciathis.java
+++ src/Maratona_java_javacore/Bintroducao/metodos/test/EstudanteTest02_MetodosReferenciathis.java
@@ -1,17 +1,10 @@
 package Maratona_java_javacore.Bintroducao.metodos.test;
-
 import Maratona_java_javacore.Bintroducao.metodos.dominio.Estudante;
 
 public class EstudanteTest02_MetodosReferenciathis {
     public static void main(String[] args) {
-        //Nesse tópico iremos aprender e entender o funcionamento do comando this, mas antes disso
-        //Vamos pensar, por que temos que chamar o metodo imprime() sendo que todas as vezes temos que ter um parametro de referencia para copiar o endereço dos objetos referentes a Classe Estudante para acessar seus valores?
-        //Por que não colocar o metodo imprime() dentro da Classe Estudante e lá mesmo fazermos a impressão sem precisar buscar dado algum de qualquer objeto ja criado, ja que assim que forem criados ja terão o imprime como parte de seu escopo?
-        //Ou seja, é isso que vamos fazer agora hehe
-        //Vamos entrar na Classe Estudante presente no dominio e adicionar a seu escopo nosso metodo de imprimir dados.
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
-
 
         estudante01.nome = "Marcos";
         estudante01.idade = 19;
@@ -21,12 +14,13 @@ public class EstudanteTest02_MetodosReferenciathis {
         estudante02.idade = 16;
         estudante02.sexo = 'F';
 
-        //Antes podiamos apenas chamar estudante0X.nome ou estudante0X.idade ou estudante0X.sexo, mas agora hehe, da uma olhada
+        //Antes podiamos apenas chamar estudante0X.nome ou estudante0X.idade ou estudante0X.sexo
+        //mas agora hehe, da uma olhada
         System.out.println("JEITO MAIS MODERNO E FUTURISTICO AINDA DE IMPRIMIR INFORMAÇÕES HEHE :)");
-        estudante01.imprime(); //Podemos chamar não apenas variaveis primitivas mas tambem funções presentes no escopo de nosso objeto
+        //Podemos chamar não apenas variaveis primitivas
+        //mas tambem metodos presentes no escopo da classe do nosso objeto
+        estudante01.imprime();
         estudante02.imprime();
-
-        //Agora vamos aprender sobre Varargs -> Calculadora
     }
 
 }

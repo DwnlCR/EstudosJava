4bb62046cb4b2bbc11e04c24fe281ecbe8ed92b7
Dwnl <danielccbcoelho@gmail.com>
2026-02-27 19:50:27 -0300

    commit

--- src/Maratona_java_javacore/Dconstrutores/test/SeriesTest01.java
+++ src/Maratona_java_javacore/Dconstrutores/test/SeriesTest01.java
@@ -3,13 +3,13 @@ import Maratona_java_javacore.Dconstrutores.dominio.Series;
 
 public class SeriesTest01 {
     public static void main(String[] args) {
-        //Series serie = new Series();
+        Series serie1 = new Series();
         //Apresentação de erro apos a adição de parametros no construtor, Series serie = new Series();
         //não funciona mais por que o construtor agora exige valores em vez de aceitar ser construido vazio
-        Series serie = new Series("The Big Bang Theory","Comedia", 80, 2018);
-        serie.imprime();
-        //Exemplo de sobrecarga de Construtor para preguiçoso que não quer por o ano da serie
-        Series serie2 = new Series("Friends", "Comedia", 236);
+        Series serie2 = new Series("The Big Bang Theory","Comedia", 80);
         serie2.imprime();
+        //Sobrecarga de Construtor
+        Series serie3 = new Series("Friends", "Comedia", 236, 2018);
+        serie3.imprime();
     }
 }

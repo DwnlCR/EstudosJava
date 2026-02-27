a63e35caa2e45481d03ec084bf03fdecefe3e5d0
Dwnl <danielccbcoelho@gmail.com>
2026-02-27 16:18:49 -0300

    commit

--- src/Maratona_java_javacore/Bintroducao/metodos/test/PessoaTest01modificadoresdeacessoegetset.java
+++ src/Maratona_java_javacore/Bintroducao/metodos/test/PessoaTest01modificadoresdeacessoegetset.java
@@ -1,17 +1,27 @@
 package Maratona_java_javacore.Bintroducao.metodos.test;
-
 import Maratona_java_javacore.Bintroducao.metodos.dominio.Pessoa;
-
 public class PessoaTest01modificadoresdeacessoegetset {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        //pessoa.nome = "Dafiny"; //Não conseguimos mais guardar valores dentro dos atributos da Classe pessoa, então vamos la na Classe para fazer uma maracutaia da boa pra gente voltar a ter acesso a esses atributos
-        //pessoa.idade = 23;
-        pessoa.setNome("Dafiny");
+        //Não conseguimos mais guardar valores dentro dos atributos da Classe pessoa dessa forma
+        //Tudo isso por que agora os atributos são privados
+        pessoa.nome = "Dafiny";
+        pessoa.idade = 23;
+        //Então vamos la na Classe Pessoa para fazer uma maracutaia da boa
+        //pra gente voltar a ter acesso a esses atributos (setters)
+
+        //Não conseguimos mais pegar e mostrar valores dentro dos atributos da Classe pessoa dessa forma
+        //Tudo isso por que agora os atributos são privados
+        System.out.println(pessoa.nome);
+        System.out.println(pessoa.idade);
+        //então vamos la na Classe para fazer uma maracutaia da boa
+        //pra gente voltar a ter acesso a esses atributos (getters)
+
+
+        //Agora podemos atribuir valores dentro dos atributos privados atraves do nosso metodo set
+        pessoa.setNome("Dafiny"); //Metodo set para atribuir valor
         pessoa.setIdade(17);
-        //System.out.println(pessoa.nome);
-        //System.out.println(pessoa.idade);Não conseguimos mais pegar e mostrar valores dentro dos atributos da Classe pessoa, então vamos la na Classe para fazer uma maracutaia da boa pra gente voltar a ter acesso a esses atributos
-        //Agora podemos acessar os valores dentro dos atributos privados atravez do nosso metodo get
+        //Agora podemos acessar os valores dentro dos atributos privados atraves do nosso metodo get
         System.out.println(pessoa.getNome());
         System.out.println(pessoa.getIdade());
 

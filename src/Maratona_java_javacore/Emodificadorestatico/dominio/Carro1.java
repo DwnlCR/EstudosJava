2a02e51ca2469bc154b8f3173af486313a5caf8f
Dwnl <danielccbcoelho@gmail.com>
2026-02-28 03:58:21 -0300

    commit

--- src/Maratona_java_javacore/Emodificadorestatico/dominio/Carro1.java
+++ src/Maratona_java_javacore/Emodificadorestatico/dominio/Carro1.java
@@ -3,33 +3,30 @@ package Maratona_java_javacore.Emodificadorestatico.dominio;
 public class Carro1 {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite = 250; //É aqui que faremos nossa mudança, basicamente iremos adicionar um "static" entre public e double, mas o que que esse modificador static faz mermo ;-;?, bom, nosso modificador static basicamente faz com que a velocidade limite pertença a classe principal Carro, n pertence mais a nenhum objeto criado a partir da classe Carro, agora basicamente qualquer objeto criado a partir da classe carro obrigatoriamente tem sua velocidade limite igual a velocidade limite de qualquer outro carro tambem criado por meio dessa classe || transformar em publico dps para exemplificar
+    private double velocidadeLimite = 250;
 
     public Carro1(String nome, double velocidadeMaxima){
         this.nome = nome;
         this.velocidadeMaxima = velocidadeMaxima;
     }
-
     public void imprime(){
         System.out.println("---------------------------");
         System.out.println("Nome: "+this.nome);
         System.out.println("Velocidade Maxima: "+this.velocidadeMaxima);
-        System.out.println("Velocidade Limite: "+ Carro1.velocidadeLimite); //***aqui não devemos mais usar o this.velocidadeLimite e sim o Carro.velocidadeLimite ja que essa velocidade n vale mais para apenas um objeto e sim para todos que forem criados
+        System.out.println("Velocidade Limite: "+ this.velocidadeLimite); //*aqui não devemos
+        //mais usar o this.velocidadeLimite e sim o Carro.velocidadeLimite ja que essa velocidade
+        //n vale mais para apenas um objeto e sim para todos que forem criados
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public double getVelocidadeMaxima() {
         return velocidadeMaxima;
     }
-
-    public void setVelocidadeMaxima(double velocidadeMxima) {
-        this.velocidadeMaxima = velocidadeMxima;
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
     }
 }
--- src/Maratona_java_javacore/Emodificadorestatico/test/Carro1Test01.java
+++ src/Maratona_java_javacore/Emodificadorestatico/test/Carro1Test01.java
@@ -1,5 +1,4 @@
 package Maratona_java_javacore.Emodificadorestatico.test;
-
 import Maratona_java_javacore.Emodificadorestatico.dominio.Carro1;
 
 public class Carro1Test01 {
@@ -7,12 +6,23 @@ public class Carro1Test01 {
         Carro1 carro1 = new Carro1("BMW", 280);
         Carro1 carro2 = new Carro1("Mercedes", 275);
         Carro1 carro3 = new Carro1("Audi", 290);
-        //Todos os 3 objetos criados acima tem algo em comum que é a lei de que todos esses carros não podem passar da sua velocidade limite em seus contadores de 250, porem em todos eles sua velocidadeMaxima é > velocidadeLimite, e ate então esta tudo bem, mas temos um problema
-        //Queremos fazer com que a velocidade limite seja a mesma para os 3 carros para q seja justo então vamos ao codigo fonte para ver isso
-        //carro1.setVelocidadeLimite(270); //Eu posso apenas vir aqui e editar a velocidade limite da minha BMW fazendo com que ela esteja fora da lei de padronização de limite de velocidade, poderiamos apenas retirar o nosso set que acessa os valores e pode altera-los como fizemos em aulas passadas, mas tambem temos uma forma nova, muito melhor e muito mais eficaz de fazer isso, e é nessa aula que iremos aprender isso
-        //Agora que fizemos as ediçoes necessarias teremos que, se quisermos alterar a velocidade limite de um carro teremos que fazer o mesmo com os outros tambem, afinal isso tem q ser justo, e faremos essas alterações todas de uma vez somente usando o comando abaixo com as alterações feitas na classe principal
-        Carro1.velocidadeLimite = 270; //Isso vai dar erro pois atualmente nossa velocidadeLimite esta privada, vamos transforma-la em publica
+        //Todos os 3 objetos criados acima tem algo em comum
+        //que é a lei de que todos esses carros não podem passar da sua velocidade limite
+        //em seus contadores de 250, porem em todos eles sua velocidadeMaxima é > velocidadeLimite
+        //e ate então esta tudo bem, mas temos um problema
+        //Queremos fazer com que a velocidade limite seja a mesma para os 3 carros
+        //para q seja justo então vamos ao codigo fonte para ver isso
+        //carro1.setVelocidadeLimite(270); //Eu posso apenas vir aqui e editar a velocidade limite da minha BMW
+        //fazendo com que ela esteja fora da lei de padronização de limite de velocidade
+        //poderiamos apenas retirar o nosso set que acessa os valores e pode altera-los como fizemos em aulas passadas
+        //mas tambem temos uma forma nova, muito melhor e muito mais eficaz de fazer isso
+        //e é nessa aula que iremos aprender isso
 
+        //Agora que fizemos as ediçoes necessarias teremos que, se quisermos alterar a velocidade limite de um carro
+        //teremos que fazer o mesmo com os outros tambem, afinal isso tem q ser justo
+        //e faremos essas alterações todas de uma vez somente usando o comando abaixo com as alterações feitas na classe principal
+        Carro1.velocidadeLimite = 270; //Isso vai dar erro pois atualmente nossa velocidadeLimite esta privada
+        //vamos transforma-la em publica
         carro1.imprime();
         carro2.imprime();
         carro3.imprime();

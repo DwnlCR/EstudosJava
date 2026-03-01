19036faae28010cf8f7dc1c547a065cbe266ed79
Dwnl <danielccbcoelho@gmail.com>
2026-03-01 03:50:11 -0300

    commit

--- src/Maratona_java_javacore/Fassociacao/dominio/Participante.java
+++ src/Maratona_java_javacore/Fassociacao/dominio/Participante.java
@@ -1,5 +1,4 @@
 package Maratona_java_javacore.Fassociacao.dominio;
-
 public class Participante {
     private String nome;
     private Equipe equipe;
@@ -7,29 +6,23 @@ public class Participante {
     public Participante (String nome) {
         this.nome = nome;
     }
-
     public void imprime() {
         System.out.println(this.nome);
         if (equipe != null) {
             System.out.println(equipe.getNome());
         }
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    //Da forma em que esta agora os jogadores são obrigados a declarar seu time inicialmente, mas ao adicionarmos nosso getter e setter para time ele pode escolher depois que time ele pertence
-
-
+    //Da forma em que esta agora os jogadores são obrigados a declarar seu time inicialmente
+    //mas ao adicionarmos nosso getter e setter para time ele pode escolher depois que time ele pertence
     public Equipe getEquipe() { //O retorno será um valor do tipo Objeto Time
         return equipe;
     }
-
     public void setEquipe(Equipe equipe) { //Para que seja atribuido aos atributos privados iniciais o usuario deve digitar algo coerente pedido pelo Objeto Time para que esse valor seja atribuido sem dar erros
         this.equipe = equipe;
     }
